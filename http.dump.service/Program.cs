b70e96e6d7676804d019e0ec02b457209e1000ed
Michael <michael_koch@hugoboss.com>
2021-12-16 14:37:31 +0100

    sdfsd

--- http.dump.service/Program.cs
+++ http.dump.service/Program.cs
@@ -1,8 +1,5 @@
 using http.dump.service.middleware;
 using http.dump.service.repositories;
-DumpRepository.BASE_DIRECTORY = @"c:\temp\dumps";
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
